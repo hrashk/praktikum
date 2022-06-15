0db0932d487001190037a910d969b3417aaa3095
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-06-15 18:44:37 +0300

    handle minimum price

--- src/main/java/ya/praktikum/delivery/Calculator.java
+++ src/main/java/ya/praktikum/delivery/Calculator.java
@@ -3,10 +3,14 @@ package ya.praktikum.delivery;
 import java.math.BigDecimal;
 
 public class Calculator {
+
+    public static final BigDecimal MINIMUM_PRICE = BigDecimal.valueOf(400);
+
     public BigDecimal computePrice(Distance distance, Size size, Durability durability, Workload workload) {
-        return distance.computePrice()
+        BigDecimal price = distance.computePrice()
                 .add(size.price())
                 .add(durability.price())
                 .multiply(workload.factor());
+        return price.max(MINIMUM_PRICE);
     }
 }
--- src/test/java/ya/praktikum/delivery/CalculatorTest.java
+++ src/test/java/ya/praktikum/delivery/CalculatorTest.java
@@ -14,4 +14,11 @@ class CalculatorTest {
         BigDecimal price = calculator.computePrice(Distance.of("1"), Size.SMALL, Durability.FRAGILE, Workload.NORMAL);
         assertEquals(BigDecimal.valueOf(450), price);
     }
+
+    @Test
+    public void below400() {
+        Calculator calculator = new Calculator();
+        BigDecimal price = calculator.computePrice(Distance.of("1"), Size.SMALL, Durability.STURDY, Workload.NORMAL);
+        assertEquals(BigDecimal.valueOf(400), price);
+    }
 }
